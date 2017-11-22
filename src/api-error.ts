7d978cb25354377df3e4b8a192a9998c105940b1
Frederick Gnodtke <frederick@gnodtke.net>
2017-11-22 16:07:02 +0100

    WIP: Add Api utilities

--- src/api-error.ts
+++ src/api-error.ts
@@ -3,13 +3,11 @@ import { ApiFailResponse } from "./api-response";
 export class ApiError extends Error {
     private statusCode: number;
     private response: ApiFailResponse;
-    private originalError: Error;
 
-    constructor(statusCode: number, response: ApiFailResponse, originalError: Error) {
+    constructor(statusCode: number, response: ApiFailResponse) {
         super();
         Error.captureStackTrace(this, ApiError);
         this.statusCode = statusCode;
         this.response = response;
-        this.originalError = originalError;
     }
 }
--- src/controller.ts
+++ src/controller.ts
@@ -4,6 +4,7 @@ import { Route } from "./route";
 import { Parameters } from "./parameters";
 import { ApiError } from "./api-error";
 import { compile } from "path-to-regexp";
+import { ApiSuccessResponse, ApiFailResponse } from "./api-response";
 
 export interface ControllerOptions {
 
@@ -19,10 +20,37 @@ export class Controller {
         return this;
     }
 
-    public async wrappedFetch<ReturnType>(route: Route, parameters: Parameters, body: any): ApiResponse<ReturnType> {
+    public async wrappedFetch<ReturnType>(route: Route, parameters: Parameters, body: any): ReturnType {
         const url = compile(route.url)(parameters);
-        const response = await fetch(url, {
-            body: JSON.
-        });
+        const headers = new Headers();
+        headers.append("content-type", "application/json");
+        try {
+            const response = await fetch(url, {
+                body: JSON.stringify(body),
+                headers,
+                method: route.method,
+            });
+            const json = await response.json();
+            if (response.ok) {
+                const successResponse: ApiSuccessResponse<ReturnType> = json;
+                return successResponse.data;
+            }
+            const failResponse: ApiFailResponse = json;
+            const error = new ApiError(response.status, failResponse);
+            if (this.errorHandler) {
+                this.errorHandler(error);
+            }
+            if (this.throwOnError) {
+                throw error;
+            }
+            return;
+        } catch(error) {
+            if (this.errorHandler) {
+                this.errorHandler(error);
+            }
+            if (this.throwOnError) {
+                throw error;
+            }
+        }
     }
 }
