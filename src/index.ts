99e9ed064a015469ea2d459ed85876cd7d11f6b5
Frederick Gnodtke <frederick@gnodtke.net>
2018-01-02 21:07:56 +0100

    Add missing export for precompute

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export { configureController } from "./configure";
 export { ApiError } from "./types";
 export * from "./validators";
 export { is } from "./validation";
-export { dump, createScope, scope, Scope, populate, specify } from "./scope";
+export { dump, createScope, scope, Scope, populate, specify, precompute } from "./scope";
 export { schemaFrom } from "./schema-generator";
 export { transform } from "./transform";
 export { AuthorizationMode, AuthorizationChecker, AuthorizationOptions, auth, noauth } from "./authorization";
